88288b84d81971dcf27c8a4949a24b6789c09bef
Lebeauvilain <diallo.oumarpapa@gmail.com>
2024-01-26 15:24:34 +0100

    Program et context ajouté

---
+++ Api/MydataBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Api
+{
+    public class MydataBase :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // on va lui donner les information de connection de notre base de donne
+            optionsBuilder.UseSqlServer("server=DESKTOP-2S6TJSU\\SQLEXPRESS;Database=GestionProduitNew;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        //public DbSet<Produit> Produits { get; set; }
+
+    }
+}
