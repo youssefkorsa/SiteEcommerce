7376a019b627628d8aaec619295e40448d52c732
Lebeauvilain <diallo.oumarpapa@gmail.com>
2024-01-29 08:52:03 +0100

    Les controleurs

---
+++ Api/Controlers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Api.Models;
+using Api.Service;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controlers
+{
+    [Route("api/Products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // Affichage du liste des produit depuis la base de données
+        [Route("")]
+        [HttpGet]
+        public List<Product> GetProducts() 
+        { 
+            ServiceProduct serviceProduct = new ServiceProduct(new MydataBase());
+            var products = serviceProduct.GetProducts();
+            return products; 
+        }
+        // Création d'un produit dans notre base de données 
+        [Route("AddProduct")]
+        [HttpGet]
+        public Product AddProduct(Product product)
+        {
+            ServiceProduct serviceProduct = new ServiceProduct(new MydataBase());
+            Product producttoAdd = serviceProduct.Addproduct(product);
+            return producttoAdd;
+        }
+
+
+        
+    }
+}
